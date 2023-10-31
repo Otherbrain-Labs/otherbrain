789eab10c560c12af03d30cab68bfa78020ac287
psugihara <peter@campsh.com>
2023-10-31 10:59:21 -0700

    i think data-table view is better

---
+++ components/catalog-table/columns.tsx
@@ -0,0 +1,104 @@
+"use client";
+
+import { loadModels } from "@/app/page";
+import { loadModels as loadAuthorModels } from "@/app/[authorSlug]/page";
+
+import { Column, ColumnDef } from "@tanstack/react-table";
+import { Button } from "../ui/button";
+import {
+  ArrowDown,
+  ArrowUp,
+  ArrowUpDown,
+  ChevronDown,
+  ChevronUp,
+  ChevronsUpDown,
+} from "lucide-react";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
+import Star from "../ui/star";
+
+type Models =
+  | Awaited<ReturnType<typeof loadModels>>
+  | Awaited<ReturnType<typeof loadAuthorModels>>;
+
+type Model = Models[number];
+
+const dateFormatter = new Intl.DateTimeFormat("en-US", {
+  year: "numeric",
+  month: "2-digit",
+  day: "2-digit",
+});
+
+function SortHeader({
+  column,
+  title,
+}: {
+  column: Column<Model>;
+  title: String;
+}) {
+  return (
+    <Button
+      variant="link"
+      onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+      className="w-full justify-start p-0 relative group"
+    >
+      <span className="absolute left-0 flex items-center">
+        <span
+          className={cn({
+            "font-bold": column.getIsSorted(),
+          })}
+        >
+          {title}
+        </span>
+        {!column.getIsSorted() && (
+          <ArrowUpDown className="ml-2 h-4 w-4 opacity-0 group-hover:opacity-50" />
+        )}
+        {column.getIsSorted() === "desc" && (
+          <ArrowUp className="ml-2 h-4 w-4" />
+        )}
+        {column.getIsSorted() === "asc" && (
+          <ArrowDown className="ml-2 h-4 w-4" />
+        )}
+      </span>
+    </Button>
+  );
+}
+
+export const columns: ColumnDef<Model>[] = [
+  {
+    accessorKey: "name",
+    header: ({ column }) => <SortHeader column={column} title="Name" />,
+    cell: ({ row }) => {
+      const model = row.original;
+      const href = `/${model.author.slug}/${model.slug}`;
+      return (
+        <Link className="hover:underline" href={href}>
+          {model.name}
+        </Link>
+      );
+    },
+  },
+  {
+    accessorKey: "stars",
+    header: "Stars",
+    cell: ({ row }) => {
+      const model = row.original;
+      return (
+        <div className="flex items-center">
+          <Star filled />
+          <span className="ml-1">{4.96}</span>
+        </div>
+      );
+    },
+  },
+  {
+    accessorKey: "datePublished",
+    header: ({ column }) => <SortHeader column={column} title="Published" />,
+    cell: ({ row }) => {
+      const model = row.original;
+      const date = model.datePublished;
+      const formatted = dateFormatter.format(date);
+      return <span>{formatted}</span>;
+    },
+  },
+];
---
+++ components/catalog-table/data-table.tsx
@@ -0,0 +1,111 @@
+"use client";
+
+import {
+  ColumnDef,
+  ColumnFiltersState,
+  SortingState,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+
+interface DataTableProps<TData, TValue> {
+  columns: ColumnDef<TData, TValue>[];
+  data: TData[];
+}
+
+export function DataTable<TData, TValue>({
+  columns,
+  data,
+}: DataTableProps<TData, TValue>) {
+  const [sorting, setSorting] = useState<SortingState>([
+    { id: "datePublished", desc: true },
+  ]);
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
+
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    onColumnFiltersChange: setColumnFilters,
+    getFilteredRowModel: getFilteredRowModel(),
+    onSortingChange: setSorting,
+    getSortedRowModel: getSortedRowModel(),
+    state: {
+      sorting,
+      columnFilters,
+    },
+  });
+
+  return (
+    <div>
+      {data.length > 5 && (
+        <div className="flex items-center pb-4">
+          <Input
+            placeholder="Filter by model name..."
+            value={(table.getColumn("name")?.getFilterValue() as string) ?? ""}
+            onChange={(event) =>
+              table.getColumn("name")?.setFilterValue(event.target.value)
+            }
+            className="max-w-sm"
+          />
+        </div>
+      )}
+      <Table>
+        <TableHeader>
+          {table.getHeaderGroups().map((headerGroup) => (
+            <TableRow key={headerGroup.id}>
+              {headerGroup.headers.map((header) => {
+                return (
+                  <TableHead key={header.id}>
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext()
+                        )}
+                  </TableHead>
+                );
+              })}
+            </TableRow>
+          ))}
+        </TableHeader>
+        <TableBody>
+          {table.getRowModel().rows?.length ? (
+            table.getRowModel().rows.map((row) => (
+              <TableRow
+                key={row.id}
+                data-state={row.getIsSelected() && "selected"}
+              >
+                {row.getVisibleCells().map((cell) => (
+                  <TableCell key={cell.id}>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))
+          ) : (
+            <TableRow>
+              <TableCell colSpan={columns.length} className="h-24 text-center">
+                No results.
+              </TableCell>
+            </TableRow>
+          )}
+        </TableBody>
+      </Table>
+    </div>
+  );
+}
