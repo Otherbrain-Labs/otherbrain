07ffc9f39c2063de1224150e3c9af1f88e4c0677
psugihara <peter@campsh.com>
2023-11-02 09:25:36 -0700

    cite data sources

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
 import prisma from "@/lib/prisma";
 import Catalog from "@/components/catalog";
+import Link from "next/link";
+import { Arrow } from "@radix-ui/react-dropdown-menu";
+import { ArrowUpRight, ArrowUpRightSquare } from "lucide-react";
 
 export async function loadModels() {
   const models = await prisma.model.findMany({
@@ -23,8 +26,47 @@ export default async function Home() {
         <p className="text-sm pt-4 pb-0 max-w-lg">
           Browse reviews and benchmarks to find the best model for your use
           case. Write reviews and share your experiences. Data graciously (open)
-          sourced from the Open LLM Leaderboard, Hugging Face, Simon Willison,
-          TheBloke.
+          sourced from the{" "}
+          <Link
+            href="https://huggingface.co/spaces/HuggingFaceH4/open_llm_leaderboard"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:underline group"
+          >
+            Open LLM Leaderboard
+            <ArrowUpRight className="hidden h-3 w-3 group-hover:inline-block" />
+          </Link>
+          ,{" "}
+          <Link
+            href="https://huggingface.co/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:underline group"
+          >
+            Hugging Face
+            <ArrowUpRight className="hidden h-3 w-3 group-hover:inline-block" />
+          </Link>
+          ,{" "}
+          <Link
+            href="https://github.com/simonw/scrape-huggingface-models/blob/main/TheBloke.json"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:underline group"
+          >
+            Simon Willison
+            <ArrowUpRight className="hidden h-3 w-3 group-hover:inline-block" />
+          </Link>
+          ,{" "}
+          <Link
+            href="https://huggingface.co/TheBloke"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:underline group"
+          >
+            TheBloke
+            <ArrowUpRight className="hidden h-3 w-3 group-hover:inline-block" />
+          </Link>
+          .
         </p>
         <Catalog models={models} />
       </div>
