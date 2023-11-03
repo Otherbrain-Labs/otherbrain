381f9350d8354f5a1a0d2f17b42cd96f5c103c76
psugihara <peter@campsh.com>
2023-11-03 07:36:21 -0700

    try tooltip

--- app/[authorSlug]/[modelSlug]/scores.tsx
+++ app/[authorSlug]/[modelSlug]/scores.tsx
@@ -3,44 +3,29 @@ import type { Model } from "./page";
 import { ScoreKey, scoreText, scoreDescription } from "@/components/scores";
 
 import {
-  Collapsible,
-  CollapsibleContent,
-  CollapsibleTrigger,
-} from "@/components/ui/collapsible";
-import { QuestionMarkIcon } from "@radix-ui/react-icons";
-import { InfoIcon } from "lucide-react";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
-import Link from "next/link";
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 export default function Scores({ model }: { model: Model }) {
   const filtered = Object.keys(scoreText).filter(
     (key) => model[key as ScoreKey]
   );
   return (
-    <Popover>
-      <div className="max-w-xl flex space-x-5">
-        {filtered.map((key) => (
-          <div className="max-w-[200px]" key={key}>
-            <div className="text-sm font-medium">{key}</div>
-            <div className="text-left text-xl">{model[key as ScoreKey]}</div>
-          </div>
-        ))}
-        <PopoverTrigger asChild>
-          {/* info button */}
-          <Button
-            variant="outline"
-            className="h-6 w-6 rounded-full p-1 text-xs font-bold"
-          >
-            ?
-          </Button>
-        </PopoverTrigger>
-      </div>
+    <Tooltip>
+      <TooltipTrigger asChild>
+        <div className="max-w-xl flex space-x-5">
+          {filtered.map((key) => (
+            <div className="max-w-[200px]" key={key}>
+              <div className="text-sm font-medium">{key}</div>
+              <div className="text-left text-xl">{model[key as ScoreKey]}</div>
+            </div>
+          ))}
+        </div>
+      </TooltipTrigger>
 
-      <PopoverContent className="space-y-2 w-100 max-w-md flex flex-col">
+      <TooltipContent className="space-y-2 w-100 max-w-md flex flex-col">
         {filtered.map((key) => (
           <div className="text-xs" key={key}>
             <span className="font-medium underline">
@@ -52,7 +37,7 @@ export default function Scores({ model }: { model: Model }) {
             </span>
           </div>
         ))}
-      </PopoverContent>
-    </Popover>
+      </TooltipContent>
+    </Tooltip>
   );
 }
