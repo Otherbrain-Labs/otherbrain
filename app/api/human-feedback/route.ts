77e967dcf12c15755e9dae603229ebfc12b507aa
psugihara <peter@campsh.com>
2023-11-13 10:31:13 -0800

    human feedback api

---
+++ app/api/human-feedback/route.ts
@@ -0,0 +1,40 @@
+import { z } from "zod";
+import prisma from "@/lib/prisma";
+
+const HumanFeedback = z.object({
+  messages: z.array(
+    z.object({
+      fromUser: z.boolean(),
+      text: z.string(),
+    })
+  ),
+  modelName: z.string(),
+  promptTemplate: z.string(),
+  lastSystemPrompt: z.string().optional(),
+  client: z.string(),
+});
+
+export async function POST(request: Request) {
+  const body = await request.json();
+  const result = HumanFeedback.parse(body);
+
+  // save with prisma
+  const humanFeedback = await prisma.humanFeedback.create({
+    data: {
+      modelName: result.modelName,
+      promptTemplate: result.promptTemplate,
+      lastSystemPrompt: result.lastSystemPrompt,
+      client: result.client,
+      messages: {
+        create: result.messages.map((message) => ({
+          fromUser: message.fromUser,
+          text: message.text,
+        })),
+      },
+    },
+  });
+
+  return new Response(JSON.stringify({ id: humanFeedback.id }), {
+    headers: { "content-type": "application/json" },
+  });
+}
