749bb5c4ecba8bdaecb525e1c1c2cf2690b93daa
psugihara <peter@campsh.com>
2023-11-13 14:23:38 -0800

    markdown support, iterative model finder

--- app/api/human-feedback/route.ts
+++ app/api/human-feedback/route.ts
@@ -18,9 +18,23 @@ export async function POST(request: Request) {
   const body = await request.json();
   const result = HumanFeedback.parse(body);
 
+  // try to find model to link to feedback by iteratively removing extensions from modelName
+  const parts = result.modelName.split(".");
+  var model = null;
+  while (parts.length > 0 && model == null) {
+    model = await prisma.model.findFirst({
+      where: {
+        slug: parts.join("."),
+      },
+      select: { id: true },
+    });
+    parts.pop();
+  }
+
   // save with prisma
   const humanFeedback = await prisma.humanFeedback.create({
     data: {
+      modelId: model?.id,
       modelName: result.modelName,
       promptTemplate: result.promptTemplate,
       lastSystemPrompt: result.lastSystemPrompt,
--- app/human-feedback/[id]/page.tsx
+++ app/human-feedback/[id]/page.tsx
@@ -1,10 +1,18 @@
 import prisma from "@/lib/prisma";
+import Link from "next/link";
 import { notFound } from "next/navigation";
+import Markdown from "react-markdown";
+import remarkGfm from "remark-gfm";
 
 export default async function Home({ params }: { params: { id: string } }) {
   const humanFeedback = await prisma.humanFeedback.findUnique({
     where: { id: params.id },
-    include: { messages: true },
+    include: {
+      messages: true,
+      model: {
+        include: { author: true },
+      },
+    },
   });
 
   if (!humanFeedback) {
@@ -12,9 +20,10 @@ export default async function Home({ params }: { params: { id: string } }) {
   }
 
   const messages = humanFeedback.messages.sort((a, b) => a.index - b.index);
+  const { model } = humanFeedback;
 
   return (
-    <div className="mt-6 max-w-lg">
+    <div className="mt-6 max-w-xl mb-20">
       <div className="mb-6">
         <div className="flex items-center justify-between">
           <h1 className="text-2xl md:text-5xl mr-2 font-semibold">
@@ -23,19 +32,35 @@ export default async function Home({ params }: { params: { id: string } }) {
         </div>
         <div className="flex items-center mt-1.5">
           <div className="text-sm">
-            {humanFeedback.createdAt.toDateString()}, {humanFeedback.modelName}
+            {humanFeedback.createdAt.toDateString()},{" "}
+            {model && (
+              <>
+                <Link href={`/${model.author.slug}/${model.slug}`}>
+                  {model.name}
+                </Link>
+                ,{" "}
+              </>
+            )}
+            {humanFeedback.modelName}
           </div>
         </div>
       </div>
 
       <div className="mt-4 border border-dashed rounded p-4 space-y-2 text-xs">
-        <div className="text-bold">System prompt</div>
-        <div>{humanFeedback.lastSystemPrompt}</div>{" "}
+        <div className="font-bold">System prompt</div>
+        <Markdown className="prose" remarkPlugins={[remarkGfm]}>
+          {humanFeedback.lastSystemPrompt}
+        </Markdown>
       </div>
-      <div className="mt-4 border border-dashed rounded p-4 space-y-2 text-xs">
+      <div className="mt-4 border border-dashed rounded p-4 space-y-4 text-xs">
         {messages.map((message) => (
-          <div className="flex">
-            {message.fromUser ? "Human" : "Bot"}: {message.text}
+          <div key={message.id} className="space-y-2">
+            <div className="font-bold">
+              {message.fromUser ? "Human" : "Bot"}
+            </div>
+            <Markdown className="prose" remarkPlugins={[remarkGfm]}>
+              {message.text}
+            </Markdown>
           </div>
         ))}
       </div>
